c77c5da44046407c8717f4506b5ba110e462b5be
Pardhu505 <168645593+Pardhu505@users.noreply.github.com>
2025-06-19 13:49:29 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const departmentData = {
+  "Soul Centre": {
+    "Soul Central": ["Atia", "Akhilesh Mishra"],
+    "Field Team": ["Siddharth Gautam", "Sai Kiran Gurram", "Himani Sehgal", "Pawan Beniwal"]
+  },
+  "Directors team": {
+    "Directors Team": ["Aditya Pandit", "Sravya", "Eshwar"]
+  },
+  "Campaign": {
+    "Campaign": ["S S Manoharan"]
+  },
+  "Data": {
+    "Data": ["T.Pardhasaradhi"]
+  },
+  "Media": {
+    "Media": ["Aakanksha Tandon"]
+  },
+  "Research": {
+    "Research": ["P.Srinath Rao"]
+  },
+  "DMC": {
+    "HIVE": ["Ruhi"],
+    "Digital Communication": ["Keerthana"],
+    "Digital Production": ["Bapan"]
+  }
+};
+
+export const statusOptions = ["WIP", "Completed", "Yet to Start", "Delayed"];
