23eacf356b648d67aac725b82295d2a7a26b0e5f
Pardhu505 <168645593+Pardhu505@users.noreply.github.com>
2025-06-19 13:08:38 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DailyWorkTracker from './components/DailyWorkTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <DailyWorkTracker />
+    </div>
+  );
+}
+
+export default App;
